54d38b5d085c4938e4c6c0c613779e1e35601f38
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2024-02-06 22:02:43 +0100

    fix: minor

--- app/runner/celery.sh
+++ app/runner/celery.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 python3 manage.py migrate
-celery -A exchange_currency worker -B -E -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
+celery -A django_app worker -B -E -l INFO --scheduler django_celery_beat.schedulers:DatabaseScheduler
