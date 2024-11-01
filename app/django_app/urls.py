3633a245fabf2acdc07f0449c58cf2cc9cfa413a
AlexanderZug <alexandertsygankov.unterwegs@gmail.com>
2024-11-01 16:29:11 +0100

    add mypy to ci

--- app/django_app/urls.py
+++ app/django_app/urls.py
@@ -8,16 +8,9 @@ from django_app.settings import PROJECT_NAME
 urlpatterns = [
     path("api/admin/", admin.site.urls),
     path("api/", include("api.urls")),
-]
-
-urlpatterns += (
     re_path(r"^api/media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
-)
-urlpatterns += (
-    re_path(
-        r"^api/static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}
-    ),
-)
+    re_path(r"^api/static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+]
 
 admin.site.site_header = f"{PROJECT_NAME} Administration"
 admin.site.site_title = PROJECT_NAME
